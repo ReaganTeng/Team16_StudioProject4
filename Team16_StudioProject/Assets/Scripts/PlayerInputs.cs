036ecdb76f65fff7ecc4e8a6ef11857ea0d8bc17
songggze <201266S@mymail.nyp.edu.sg>
2023-02-13 14:49:42 +0800

    Added projectile to 3rd person

--- Team16_StudioProject/Assets/Scripts/PlayerInputs.cs
+++ Team16_StudioProject/Assets/Scripts/PlayerInputs.cs
@@ -7,10 +7,12 @@ public class PlayerInputs : MonoBehaviour
 {
     public Rigidbody projectile;
     private GameObject player;
+    private Vector3 pos;
 
     void Awake()
     {
-        player = GameObject.Find("JohnLemon");
+        player = GameObject.Find("NestedParentArmature_Unpack");
+        //camera = GameObject<GetComponent>(Camera);
     }
 
     void Update()
@@ -21,11 +23,12 @@ public class PlayerInputs : MonoBehaviour
             // Instantiate the projectile at the position and rotation of this transform
             Rigidbody clone;
             clone = Instantiate(projectile, transform.position, transform.rotation);
-            // Give the cloned object an initial velocity along the current
-            // object's Z axis
-            clone.position += Vector3.up * 1.0f;
-            clone.position += player.transform.forward * 1.0f;
-            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+            pos = transform.position;
+
+            clone.position = Camera.main.transform.position;
+            //clone.position += Vector3.up * 1.0f;
+            clone.position += Camera.main.transform.forward * 3.0f;
+            clone.velocity = transform.TransformDirection(Camera.main.transform.forward * 40);
         }
     }
 
