ab3a3a84443c127a7172418033cc4f3ce13069f5
ReaganTeng <yuzhe100903@gmail.com>
2023-02-13 14:46:34 +0800

    added back fsms

---
+++ Team16_StudioProject/Assets/Scripts/FSM/GuardSearchState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GuardSearchState : GuardStateBase
+{
+    public override void EnterState(GuardStateManager guard)
+    {
+        Debug.Log("SEARCHING STATE");
+
+    }
+
+    public override void UpdateState(GuardStateManager guard)
+    {
+
+    }
+
+    public override void OnCollisionEnter(GuardStateManager guard, Collision collision)
+    {
+
+    }
+}
