4b02a041c95b2b042e12971f397cdaa9f684fd02
ReaganTeng <yuzhe100903@gmail.com>
2023-02-13 22:22:47 +0800

    Updated FSM

--- Team16_StudioProject/Assets/Scripts/FSM/GuardPatrolState.cs
+++ Team16_StudioProject/Assets/Scripts/FSM/GuardPatrolState.cs
@@ -1,38 +1,45 @@
+
 using UnityEngine;
 using UnityEngine.AI;
 
 public class GuardPatrolState : GuardStateBase
 {
     float timer;
+    private Transform[] wayp;
+    NavMeshAgent navMeshAgent;
+    int m_CurrentWaypointIndex;
+    float detectiondistance;
 
-    //public Transform[] waypoints;
-    //NavMeshAgent navMeshAgent;
-    //int m_CurrentWaypointIndex;
-
-
-    public override void EnterState(GuardStateManager guard)
+    public override void EnterState(GuardStateManager guard, Transform[] wp)
     {
         Debug.Log("PATROL STATE");
         timer = 0;
-        //navMeshAgent.SetDestination(waypoints[0].position);
+
+        //wayp[0].position = guard.waypoints[0].position;
+        //wayp[1].position = guard.waypoints[1].position;
+
+        //set first destination
+        guard.navMeshAgent.SetDestination(guard.waypoints[0].position);
+        detectiondistance = 5.0f;
 
     }
 
     public override void UpdateState(GuardStateManager guard)
     {
-        timer += 1.0f * Time.deltaTime;
-
-        if (timer > 3.0f)
+        //if player and enemy diatance is within 5
+        if (Vector3.Distance(guard.player.transform.position, guard.getgenemyPos().position) < detectiondistance)
         {
-            guard.SwitchState(guard.StationState);
+            guard.SwitchState(guard.ChaseState);
         }
 
+        Debug.Log("DISTANCE POSITION " + Vector3.Distance(guard.getgenemyPos().position, guard.player.transform.position));
 
-        //if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
-        //{
-        //    m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
-        //    navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-        //}
+
+        if (guard.navMeshAgent.remainingDistance < guard.navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % guard.waypoints.Length;
+            guard.navMeshAgent.SetDestination(guard.waypoints[m_CurrentWaypointIndex].position);
+        }
     }
 
     public override void OnCollisionEnter(GuardStateManager guard, Collision collision)
