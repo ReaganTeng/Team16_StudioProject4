a0ff7b8866451a48091cb3ba847b86b749426213
songggze <201266S@mymail.nyp.edu.sg>
2023-02-22 10:57:27 +0800

    added story before gameplay

--- Team16_StudioProject/Assets/Scripts/MainmenuInput.cs
+++ Team16_StudioProject/Assets/Scripts/MainmenuInput.cs
@@ -7,23 +7,38 @@ using UnityEngine.SceneManagement;
 public class MainmenuInput : MonoBehaviour
 {
 
-
+    private GameObject storyDialog;
+    private GameObject mainMenu;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()    
     {
 
-
-
+        storyDialog = GameObject.Find("Story");
+        mainMenu = GameObject.Find("Mainmenu");
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Begin gameplay when pressing ENTER in story dialog
+        if (mainMenu.activeSelf)
+            storyDialog.SetActive(false);
+
+        if (storyDialog.gameObject.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene("LevelScene");
+            }
+        }
+
+
     }
 
     public void StartButton()
     {
-        SceneManager.LoadScene("LevelScene");
+        storyDialog.SetActive(true);
+        mainMenu.SetActive(false);
     }
 }
