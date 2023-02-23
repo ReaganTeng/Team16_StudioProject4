1a2478bbae6354449644d14196baa88f81ca108c
songggze <201266S@mymail.nyp.edu.sg>
2023-02-23 14:48:03 +0800

    added scrolling background

--- Team16_StudioProject/Assets/Scripts/ScrollingBackground.cs
+++ Team16_StudioProject/Assets/Scripts/ScrollingBackground.cs
@@ -17,7 +17,7 @@ public class ScrollingBackground : MonoBehaviour
     void Update()
     {
         float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
-        Vector2 offset = new Vector2(x, x);
+        Vector2 offset = new Vector2(x, x * 0.2f);
         renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
     }
 }
