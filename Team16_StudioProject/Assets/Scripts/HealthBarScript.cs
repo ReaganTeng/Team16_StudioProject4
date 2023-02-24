198f21ce7bcf5908191dfaf21b85f2e38eda245d
songggze <201266S@mymail.nyp.edu.sg>
2023-02-24 10:42:04 +0800

    healthbar animation

--- Team16_StudioProject/Assets/Scripts/HealthBarScript.cs
+++ Team16_StudioProject/Assets/Scripts/HealthBarScript.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 public class HealthBarScript : MonoBehaviour
@@ -8,19 +10,65 @@ public class HealthBarScript : MonoBehaviour
     private GameObject healthBarObject;
     private Slider healthBar;
 
+    // For animation
+    private float currentValue;
+
+    private bool healthBarMoveUp;
+    private float healthBarTransform;
+    private Vector3 healthBarStartPos;
+
     // Start is called before the first frame update
     void Start()
     {
         playerStats = GameObject.Find("PlayerArmature");
         healthBarObject = GameObject.Find("Health Bar");
         healthBar = GetComponent<Slider>();
-        healthBar.maxValue = 100;
+        healthBar.maxValue = playerStats.GetComponent<PlayerStats>().health;
+        currentValue = healthBar.maxValue;
+
+        healthBarMoveUp = true;
+        healthBarTransform = 0;
+        healthBarStartPos = healthBarObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthBar.value = playerStats.GetComponent<PlayerStats>().health;
+        if (currentValue > playerStats.GetComponent<PlayerStats>().health)
+        {
+            currentValue -= 50 * Time.deltaTime;
+            if (currentValue < playerStats.GetComponent<PlayerStats>().health)
+            {
+                currentValue = playerStats.GetComponent<PlayerStats>().health;
+                healthBarTransform = 0;
+            }
+            else
+            {
+                // Handle health bar shaking animation
+                if (healthBarMoveUp)
+                {
+                    healthBarTransform += 200 * Time.deltaTime;
+
+                    if (healthBarTransform >= 5)
+                    {
+                        healthBarMoveUp = false;
+                    }
+                }
+                else
+                {
+                    healthBarTransform -= 200 * Time.deltaTime;
+
+                    if (healthBarTransform <= -5)
+                    {
+                        healthBarMoveUp = true;
+                    }
+                }
+            }
+
+
+            healthBarObject.transform.position = healthBarStartPos + (new Vector3 (0, healthBarTransform, 0));
+        }
+        healthBar.value = currentValue;
 
     }
 }
